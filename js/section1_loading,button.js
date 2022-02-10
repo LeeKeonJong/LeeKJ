5dbbb761197a04aa2885ed2596df4038235eb2c0
gjr100g <93234774+LeeKeonJong@users.noreply.github.com>
2022-02-10 16:54:43 +0900

    업데이트

--- js/section1_loading,button.js
+++ js/section1_loading,button.js
@@ -4,8 +4,7 @@ $(function(){
        
         $("#loading").fadeOut(1000);
         $("#loading").css("transform","translateY(-100%)").css("transition","1s");
-        
-
+    
         $("#section1_button").click(function(){
             $('html,body').animate({scrollTop:$(".section2").offset().top},500);
         });
--- js/section2_count.js
+++ js/section2_count.js
@@ -3,7 +3,13 @@ $(function(){
    
            
             var cnt0 = 0;
+            var cnt1 = 0;
+            var cnt2 = 0;
+            var cnt3 = 0;
             counterFn();
+            counterFn2();
+            counterFn3();
+            counterFn4();
     
     function counterFn(){
 
@@ -13,16 +19,14 @@ $(function(){
 
         var s2=$('#section2_top').offset().top;
         
-        var sec2=$('.section2').offset().top;
+        
 
-        if(ws>s2-500){
+        if(ws>s2-300){
         
        setTimeout(function(){
         id0 = setInterval(count0Fn, 100);
-        id1 = setInterval(count0Fn, 100);
-        id2 = setInterval(count0Fn, 100);
-        id3 = setInterval(count0Fn, 100);
-    },700);
+        
+    },800);
     
  }
 });
@@ -33,21 +37,107 @@ $(function(){
             }else {
                 $(".count1").text(cnt0);
                 $(".count2").text(cnt0);
-            }if (cnt0 > 60){
+            }
+        }
+
+    }
+
+
+
+    function counterFn2(){
+
+        $(window).scroll(function(){
+
+        var ws=$(this).scrollTop();
+
+        var s2=$('#section2_top').offset().top;
+        
+        
+
+        if(ws>s2-300){
+        
+       setTimeout(function(){
+        id1 = setInterval(count1Fn, 100);
+        
+    },800);
+    
+ }
+});
+        function count1Fn(){
+            cnt1++;
+            if (cnt1 > 60) {
                 clearInterval(id1);
             }else {
-                $(".count3").text(cnt0);
-            }if (cnt0 > 80) {
+                $(".count3").text(cnt1);
+            }
+        }
+
+    }
+
+
+
+    function counterFn3(){
+
+        $(window).scroll(function(){
+
+        var ws=$(this).scrollTop();
+
+        var s2=$('#section2_top').offset().top;
+        
+        
+
+        if(ws>s2-300){
+        
+       setTimeout(function(){
+        id2 = setInterval(count2Fn, 100);
+        
+    },800);
+    
+ }
+});
+        function count2Fn(){
+            cnt2++;
+            if (cnt2 > 80) {
                 clearInterval(id2);
             }else {
-                $(".count4").text(cnt0);
-            }if (cnt0 > 70) {
-                clearInterval(id3)
+                $(".count4").text(cnt2);
+            }
+        }
+
+    }
+
+
+
+    function counterFn4(){
+
+        $(window).scroll(function(){
+
+        var ws=$(this).scrollTop();
+
+        var s2=$('#section2_top').offset().top;
+        
+        
+
+        if(ws>s2-300){
+        
+       setTimeout(function(){
+        id3 = setInterval(count3Fn, 100);
+        
+    },800);
+    
+ }
+});
+        function count3Fn(){
+            cnt3++;
+            if (cnt3 > 70) {
+                clearInterval(id3);
             }else {
-                $(".count5").text(cnt0);
+                $(".count5").text(cnt3);
             }
         }
 
     }
 
 });
+            
+        
